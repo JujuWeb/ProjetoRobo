5754ea66fb0220fdf4cf67c4add0a1952116d3bf
Valessia Juliany <valessia.juliany@escolar.ifrn.edu.br>
2025-12-03 23:28:51 -0300

    Update entidades.py

--- entidades.py
+++ entidades.py
@@ -128,7 +128,7 @@ class RoboZigueZague(Robo):
         self.direcao = 1
 
         self.original_image = pygame.image.load("assets/furacao.png").convert_alpha()
-        self.original_image = pygame.transform.scale(self.original_image, (60, 60))
+        self.original_image = pygame.transform.scale(self.original_image, (52, 52))
 
         self.image = self.original_image
         self.rect = self.image.get_rect(center=(x, y))
@@ -148,7 +148,7 @@ class RoboZigueZague(Robo):
             self.direcao *= -1
 
         # Rotação do furacão
-        self.angulo = (self.angulo + 10) % 360   # quanto maior → gira mais rápido
+        self.angulo = (self.angulo + 15) % 360   # quanto maior → gira mais rápido
         self.image = pygame.transform.rotate(self.original_image, self.angulo)
 
         centro = self.rect.center
@@ -163,7 +163,7 @@ class RoboZigueZague(Robo):
 # ROBO RAPIDO
 class RoboRapido(Robo):
     def __init__(self, x, y):
-        super().__init__(x, y, velocidade=10)
+        super().__init__(x, y, velocidade=12)
 
         img = pygame.image.load("assets/estrelacadente.png").convert_alpha()
         img = pygame.transform.rotate(img, -50)
@@ -205,15 +205,15 @@ class RoboCiclico(Robo):
 
         super().__init__(x, y, velocidade=2)
 
-        self.image = pygame.image.load("assets/BluePlanet.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (60, 60))
+        self.image = pygame.image.load("assets/UfoBlue.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect(center=(x, y))
 
         self.base_x = max(40, min(x, LARGURA - 40))
         self.base_y = y
 
         self.raio = 60
-        self.vel_giro = 0.4
+        self.vel_giro = 0.3
 
         self.tabela_x = [0, 1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1]
         self.tabela_y = [-3, -2, -1, 0, 1, 2, 3, 2, 1, 0, -1, -2]
@@ -243,11 +243,12 @@ class RoboCiclico(Robo):
 # ROBO CAÇADOR 
 class RoboCacador(Robo):
     def __init__(self, x, y, jogador):
-        super().__init__(x, y, velocidade=4)
+        super().__init__(x, y, velocidade=5)
 
         self.jogador = jogador
-        self.image = pygame.image.load("assets/Nave_caçador.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (80, 80))
+        self.image = pygame.image.load("assets/nave.png").convert_alpha()
+        self.image = pygame.transform.rotate(self.image, 180)
+        self.image = pygame.transform.scale(self.image, (65, 65))
         self.rect = self.image.get_rect(center=(x, y))
 
     def atualizar_posicao(self):
@@ -255,12 +256,10 @@ class RoboCacador(Robo):
         dx = self.jogador.rect.centerx - self.rect.centerx
         dy = self.jogador.rect.centery - self.rect.centery
 
-        
         distancia = max(1, (dx**2 + dy**2) ** 0.5)
 
         self.rect.x += (dx / distancia) * self.velocidade
         self.rect.y += (dy / distancia) * self.velocidade
-
         
         if self.rect.y > ALTURA + 40:
             self.kill()
